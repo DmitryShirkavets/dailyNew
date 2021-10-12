04029c63431fc1f7d941f0df85e1a1b8f7f4a1f3
DmitryShirkavets <46999066+DmitryShirkavets@users.noreply.github.com>
2021-10-12 13:58:30 +0300

    Section Channels

--- src/components/channels/Channels.jsx
+++ src/components/channels/Channels.jsx
@@ -3,12 +3,24 @@ import s from './Channels.module.css';
 import SectionTitle from '../section-title/SectionTitle';
 import OneChannel from '../one-channel/OneChannel';
 
+import logo1 from './../../assets/img/channel1.svg';
+import logo2 from './../../assets/img/channel2.svg';
+import logo3 from './../../assets/img/channel3.svg';
+import logo4 from './../../assets/img/channel4.svg';
+import logo5 from './../../assets/img/channel5.svg';
+import logo6 from './../../assets/img/channel6.svg';
+
 function Channels() {
     return (
         <section className={s.channels}>
           <SectionTitle title="Explore Channels" />
-          <div className="block-content">
-            <OneChannel />
+          <div className={s.blockContent}>
+            <OneChannel style={{ marginBottom: "27px" }} title="FOX NEWS" logo={logo1} />
+            <OneChannel style={{ marginBottom: "39px" }} title="CW6 NEWS" logo={logo2} />
+            <OneChannel style={{ marginBottom: "39px" }} title="ABC NEWS" logo={logo3} />
+            <OneChannel style={{ marginBottom: "29px" }} title="AL JAZEERA" logo={logo4} />
+            <OneChannel style={{ marginBottom: "42px" }} title="BBC NEWS" logo={logo5} />
+            <OneChannel style={{ marginBottom: "38px" }} title="CNN NEWS" logo={logo6} />
           </div>
         </section>
     );
--- src/components/one-channel/OneChannel.jsx
+++ src/components/one-channel/OneChannel.jsx
@@ -1,9 +1,13 @@
 import s from './OneChannel.module.css';
 
+import ArticleHeader from '../article-header/ArticleHeader';
+
 function OneChannel(props) {
     return (
-        <>
-        </>
+        <div className={s.oneChannel}>
+            <img style={props.style} className={s.logo} src={props.logo} alt="" />
+            <ArticleHeader title={props.title} />
+        </div>
     );
 }
 
